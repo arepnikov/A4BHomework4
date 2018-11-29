0e7d81e3d4101511b86c29b70621aaa03cc7280c
Sir Alexsearch <aleksander.repnikov@gmail.com>
2018-11-29 18:19:25 +0100

    init + dodanie ViewModel

---
+++ app/src/main/java/com/daftmobile/android4beginners4/robots/RobotsActivity.kt
@@ -0,0 +1,32 @@
+package com.daftmobile.android4beginners4.robots
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.daftmobile.android4beginners4.robots.viewmodel.LiveDataRobotsViewModel
+import com.daftmobile.android4beginners4.robots.viewmodel.RobotsViewModel
+import kotlinx.android.synthetic.main.activity_robots.*
+
+class RobotsActivity : AppCompatActivity() {
+    private lateinit var viewModel: RobotsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_robots)
+
+        viewModel = ViewModelProviders.of(this).get(LiveDataRobotsViewModel::class.java)
+        viewModel.getRobotList().observe(this, Observer {
+            robotTextView.text = it
+        })
+
+        addRobotFab.setOnClickListener {
+            addNewItem()
+        }
+    }
+
+    private fun addNewItem() {
+        viewModel.addRobot()
+    }
+}
